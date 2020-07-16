3da58953842d54e092f6c1b012207b8b79b49405
raphzam <raphaeljasonzamora@gmail.com>
2020-07-16 14:02:15 -0400

    nav frag, add, list all

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 
 @Controller
 public class HomeController {
+    ArrayList<Song> allSongs = new ArrayList<Song>();
 
     @RequestMapping("/songform")
     public String loadSongForm(Model model){
@@ -19,10 +21,18 @@ public class HomeController {
     }
 
     @PostMapping("/confirmsongform")
-    public String loadFromSong(@Valid Song song, BindingResult result){
+    public String loadFromSong(@Valid Song song, BindingResult result, Model model){
         if (result.hasErrors()){
             return "songform";
         }
+        allSongs.add(song);
+        model.addAttribute("allSongs", allSongs);
+        return "confirmsong";
+    }
+
+    @RequestMapping("/listAllSongs")
+    public String listAllSongs(Model model){
+        model.addAttribute("allSongs", allSongs);
         return "confirmsong";
     }
 
